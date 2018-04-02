51a7d836f26f611e5f6595446959e415a9fd5dd7
Alessandro Di Maria <grrrisu@users.noreply.github.com>
2018-04-02 18:41:00 +0200

    Devise (#1)
    * add devise
    
    * nav collapse
    
    * add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  devise_for :users, path_names: {
+    sign_in: 'login', sign_out: 'logout'
+  }
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select '#login-link', 'login'
+  end
+
+  test "should see logout link" do
+    sign_in users(:john_snow)
+    get root_path
+    assert_response :success
+    assert_select '#logout-link', 'logout'
   end
 
 end
