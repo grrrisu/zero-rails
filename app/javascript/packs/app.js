2de7edfe853930b25571c0783f0ce23b9bb700f6
Alessandro Di Maria <adm@m42.ch>
2018-10-16 20:32:35 +0200

    add materialze css

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -6,5 +6,8 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import "jquery";
+
+import "../../../node_modules/materialize-css/dist/js/materialize.js";
 
 console.log("Hello World from Webpacker");
