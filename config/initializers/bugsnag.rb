add9f1ec77fd9ef95938c4994f7642d879ba62f6
Alessandro Di Maria <adm@m42.ch>
2018-11-25 21:15:32 +0100

    read bugsnag key from credentials not secrets

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,4 @@
 Bugsnag.configure do |config|
-  config.api_key = Rails.application.secrets.bugsnag_key
+  config.api_key = Rails.application.credentials.bugsnag_key
   config.notify_release_stages = ['production', 'staging']
 end
